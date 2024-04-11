d3665966a22f89d53eba013d89ec7a1214f5b207
spidugu <susighnithareddy00@gmail.com>
2024-04-11 15:10:23 +0530

    firstcommit

---
+++ devopsleraning/com.DevopsLearning/src/main/java/com/DevopsLearning/Devops.java
@@ -0,0 +1,12 @@
+package com.DevopsLearning;
+
+public class Devops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("hello suisghnitha");
+ System.out.println("welcome to devops");
+ 
+	}
+
+}
